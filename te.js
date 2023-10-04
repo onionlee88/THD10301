d0e8961d3666d6dc0511b1e312a796cf97ceb4df
onionlee88 <onionqq66@gmail.com>
2023-10-04 11:17:33 +0800

    第二次個專報告-更新

---
+++ te.js
@@ -0,0 +1,17 @@
+// let wallet = 200;
+
+// let iceCubes = 25;
+
+// let buyIce = iceCubes * 7;
+
+// let leftOverMoney = wallet - buyIce;
+
+// console.log(leftOverMoney);
+
+function calcTotalScore(chineseScore, mathScore) {
+  return chineseScore + mathScore;
+}
+
+let markTotalScore = calcTotalScore(40, 60);
+
+console.log(markTotalScore);
